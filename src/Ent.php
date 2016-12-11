03415ceac290d5963743096dc0de89d0ed55b95f
doup <asier@illarra.com>
2016-12-11 19:48:49 +0100

    Blog templates

--- src/Ent.php
+++ src/Ent.php
@@ -216,10 +216,31 @@ class Ent {
         
         if (is_home() || is_archive() || is_search()) {
             $context['posts'] =  new \Timber\PostQuery();
-            
+                        
             if (is_home()) {
                 $tpl = 'blog.twig';
             } else if (is_archive()) {
+                $q = get_queried_object();
+
+                if (is_category() || is_tag() || is_tax()) {
+                    $context['type'] = 'term';
+                    $context['term'] = Timber::get_term($q->term_taxonomy_id, $q->taxonomy);
+                } else if (is_author()) {
+                    $context['type'] = 'author';
+                    $context['author'] = new Timber\User($q->ID);
+                } else if (is_year()) {
+                    $context['type'] = 'year';
+                    $context['year'] = get_the_date(__('wp.blog.year_format'));
+                } else if (is_month()) {
+                    $context['type'] = 'month';
+                    $context['month'] = get_the_date(__('wp.blog.month_format'));
+                } else if (is_day()) {
+                    $context['type'] = 'day';
+                    $context['day'] = get_the_date(__('wp.blog.day_format'));
+                } else {
+                    $context['type'] = '';
+                }
+                
                 $tpl = 'archive.twig';
             } else if (is_search()) {
                 $tpl = 'search.twig';
